f19769c5462eaa8c593eed30c6e270020baca407
Žygimantas Bagdzevičius <zbagdzevicius@gmail.com>
2024-05-24 12:53:15 +0300

    fix feed

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -2,8 +2,18 @@ import { FeedLanding } from "@/components/blog/feed/FeedLanding";
 import { SeoHead } from "@/components/seo/SeoHead";
 import BlogLayout from "@/layouts/BlogLayout";
 import { WebPageJsonLd } from "next-seo";
+import { useEffect, useState } from "react";
 
 function BlogLanding() {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return <></>;
+  }
+
   return (
     <div className="relative">
       <SeoHead
