0a1634b448dd70008970a56ecb325ab32d7c27ca
Žygimantas Bagdzevičius <zbagdzevicius@gmail.com>
2024-07-19 16:19:51 +0300

    disable google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,10 @@
-import "../styles/globals.scss";
 import { MainLayout } from "@/layouts/MainLayout";
 import type { AppProps } from "next/app";
-import { GoogleAnalytics } from "nextjs-google-analytics";
-import { useEffect, useState } from "react";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MainLayout>
-      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </MainLayout>
   );
