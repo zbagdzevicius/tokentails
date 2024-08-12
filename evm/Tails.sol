d4482bda1df520078853f96668bd11dc7d07761d
Žygimantas Bagdzevičius <zbagdzevicius@gmail.com>
2024-08-12 15:57:59 +0300

    ERC-20: remove burning, access control

--- evm/Tails.sol
+++ evm/Tails.sol
@@ -3,22 +3,9 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.9.3/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts@4.9.3/access/AccessControlEnumerable.sol";
 
-contract TokenTails is ERC20, ERC20Burnable, AccessControlEnumerable {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    uint256 public immutable maxSupply = 500000000 * 10 ** decimals();
-
-    constructor()
-        ERC20("Token Tails", "TAILS")
-    {
-        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
-    }
-
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        uint256 amountWithDecimals = amount * 10 ** decimals();
-        require(totalSupply() + amountWithDecimals <= maxSupply, "minting would exceed max supply");
-        _mint(to, amountWithDecimals);
+contract TokenTails is ERC20 {
+    constructor() ERC20("Token Tails", "TAILS") {
+        _mint(_msgSender(), 500000000 * 10 ** decimals());
     }
 }
