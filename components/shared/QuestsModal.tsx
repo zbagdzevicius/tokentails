b1f74b8d085fa05db52f0bc627ed9c41cf04a859
Žygimantas Bagdzevičius <zbagdzevicius@gmail.com>
2024-09-18 13:39:07 +0300

    START_RUN_TAP quest

--- components/shared/QuestsModal.tsx
+++ components/shared/QuestsModal.tsx
@@ -32,6 +32,7 @@ export enum QUEST {
   START_DONZ_SQUAT = "START_DONZ_SQUAT",
   START_TAP_WARRIOR = "START_TAP_WARRIOR",
   START_STAR_AI = "START_STAR_AI",
+  START_RUN_TAP = "START_RUN_TAP",
   FOLLOW_TG_CHANNEL = "FOLLOW_TG_CHANNEL",
   FOLLOW_TG_GROUP = "FOLLOW_TG_GROUP",
   FOLLOW_X = "FOLLOW_X",
@@ -145,6 +146,16 @@ const allQuests: IQuest[] = [
     name: "Start Star AI",
     link: "https://t.me/TheStarAIBot/StarAI?startapp=KwAg1e",
     icon: "/icons/social/starai.webp",
+    reward: {
+      coins: 1000,
+    },
+  },
+  {
+    type: QuestType.PARTNERS,
+    key: QUEST.START_RUN_TAP,
+    name: "Start RUN Tap Tap",
+    link: "https://t.me/run_tap_tap_bot/RunTapTap?startapp=M04OUXXJ6787273224",
+    icon: "/icons/social/run_tap.webp",
     reward: {
       coins: 1000,
     },
