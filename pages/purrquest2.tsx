3ed3c16d84076b35ee50512ca8aaaff255089a14
Žygimantas Bagdzevičius <zbagdzevicius@gmail.com>
2024-09-22 19:19:36 +0300

    fix build

--- pages/purrquest2.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-
-const PurrQuest = dynamic(() => import("@/components/purrquest/PurrQuest"), { ssr: false });
-
-export default function Game() {
-    return <PurrQuest />;
-}
