1ec27b07431fa0b54c49081b9ff57093cebd6797
Ernest Rimkevicius <ernest20145@gmail.com>
2024-11-06 13:05:31 +0000

    Trampoline rework

--- components/Phaser/Trampoline/Trampoline.ts
+++ components/Phaser/Trampoline/Trampoline.ts
@@ -1,17 +1,16 @@
+const BOUNCE_VELOCITY = -1000;
+
 export class Trampoline {
   private scene: Phaser.Scene;
-  private bounceVelocity: number;
   private tileLayer: Phaser.Tilemaps.TilemapLayer;
   private trampolineTiles: number[];
 
   constructor(
     scene: Phaser.Scene,
     tileLayer: Phaser.Tilemaps.TilemapLayer,
-    bounceVelocity: number = -1000,
     trampolineTiles: number[]
   ) {
     this.scene = scene;
-    this.bounceVelocity = bounceVelocity;
     this.tileLayer = tileLayer;
     this.trampolineTiles = trampolineTiles;
 
@@ -29,8 +28,11 @@ export class Trampoline {
 
   private handleBounce(player: Phaser.GameObjects.GameObject) {
     const playerSprite = player as Phaser.Physics.Arcade.Sprite;
-    if (playerSprite.body!.velocity.y <= 0) {
-      playerSprite.setVelocityY(this.bounceVelocity);
+    if (
+      playerSprite.body!.velocity.y > 0 ||
+      playerSprite.body!.velocity.y <= 0
+    ) {
+      playerSprite.setVelocityY(BOUNCE_VELOCITY);
       this.scene.sound.play("powerup");
     }
     return true;
