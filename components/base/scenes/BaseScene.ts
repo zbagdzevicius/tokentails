af83acb787d1408d7d20269896d40c9087620f7f
Ernest Rimkevicius <ernest20145@gmail.com>
2024-11-21 08:31:20 +0000

    Dialogue fix

--- components/base/scenes/BaseScene.ts
+++ components/base/scenes/BaseScene.ts
@@ -4,12 +4,7 @@ import { ICat } from "@/models/cats";
 import { Scene } from "phaser";
 import { Cat, NPCJobType } from "../objects/Cat";
 import { Food } from "../objects/Food";
-import { getRandomItemFromArray } from "@/constants/utils";
-import { greetingText } from "@/constants/utils";
-import { aftrFeedText } from "@/constants/utils";
 
-const greetingMessage = getRandomItemFromArray(greetingText);
-const afterFeedMessag = getRandomItemFromArray(aftrFeedText);
 export class BaseScene extends Scene {
   platform!: Phaser.GameObjects.Rectangle;
   cat?: Cat;
@@ -23,9 +18,6 @@ export class BaseScene extends Scene {
     | Phaser.Sound.WebAudioSound
     | Phaser.Sound.NoAudioSound
     | Phaser.Sound.HTML5AudioSound;
-  speechBubble?: Phaser.GameObjects.Graphics;
-  speechBubbleText?: Phaser.GameObjects.Text;
-  private typingTimer?: Phaser.Time.TimerEvent;
 
   constructor() {
     super("BaseScene");
@@ -139,102 +131,6 @@ export class BaseScene extends Scene {
     this.physics.add.collider(this.cat.sprite, this.groundLayer);
     this.cameras.main.startFollow(this.cat.sprite);
     this.cameras.main.zoom = ZOOM;
-    setTimeout(() => {
-      this.createSpeechBubble(
-        this.cat!.sprite.x,
-        this.cat!.sprite.y - 50,
-        150,
-        35,
-        greetingMessage
-      );
-    }, 500);
-  }
-
-  createSpeechBubble(
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    quote: string | undefined
-  ) {
-    if (this.speechBubble) {
-      this.speechBubble.destroy();
-      this.speechBubbleText?.destroy();
-    }
-
-    const bubbleWidth = width;
-    const bubbleHeight = height;
-    const bubblePadding = 2;
-    const arrowHeight = bubbleHeight / 2;
-
-    const bubble = this.add.graphics({ x: x, y: y });
-
-    bubble.fillStyle(0xffffff, 1);
-    bubble.lineStyle(4, 0x565656, 1);
-    bubble.strokeRoundedRect(0, 0, bubbleWidth, bubbleHeight, 0);
-    bubble.fillRoundedRect(0, 0, bubbleWidth, bubbleHeight, 0);
-
-    const point1X = Math.floor(bubbleWidth / 7);
-    const point1Y = bubbleHeight;
-    const point2X = Math.floor((bubbleWidth / 7) * 2);
-    const point2Y = bubbleHeight;
-    const point3X = Math.floor(bubbleWidth / 7);
-    const point3Y = Math.floor(bubbleHeight + arrowHeight);
-
-    bubble.fillTriangle(point1X, point1Y, point2X, point2Y, point3X, point3Y);
-    bubble.lineStyle(2, 0x565656, 1);
-    bubble.lineBetween(point2X, point2Y, point3X, point3Y);
-    bubble.lineBetween(point1X, point1Y, point3X, point3Y);
-
-    const content = this.add.text(0, 0, "", {
-      fontFamily: "Arial",
-      fontSize: "10px",
-      color: "#000000",
-      align: "center",
-      wordWrap: { width: bubbleWidth - bubblePadding * 2 },
-    });
-
-    const b = content.getBounds();
-
-    content.setPosition(
-      bubble.x + bubbleWidth / 2 - b.width / 2,
-      bubble.y + bubbleHeight / 2 - b.height / 2
-    );
-
-    this.speechBubble = bubble;
-    this.speechBubbleText = content;
-
-    let index = 0;
-    const typingSpeed = 100;
-
-    const typeNextCharacter = () => {
-      if (!this.speechBubble || !this.speechBubbleText || !quote) {
-        return;
-      }
-
-      if (index < quote.length) {
-        this.speechBubbleText.setText(
-          this.speechBubbleText.text + quote[index]
-        );
-        index++;
-
-        const b = this.speechBubbleText.getBounds();
-        this.speechBubbleText.setPosition(
-          this.speechBubble.x + bubbleWidth / 2 - b.width / 2,
-          this.speechBubble.y + bubbleHeight / 2 - b.height / 2
-        );
-
-        this.typingTimer = this.time.delayedCall(
-          typingSpeed,
-          typeNextCharacter,
-          [],
-          this
-        );
-      }
-    };
-    typeNextCharacter();
-
-    typeNextCharacter();
   }
 
   spawnFood() {
@@ -243,21 +139,6 @@ export class BaseScene extends Scene {
     }
 
     this.food = new Food(this, 200, -400);
-
-    if (this.typingTimer) {
-      this.typingTimer.remove(false);
-      this.typingTimer = undefined;
-    }
-
-    if (this.speechBubble) {
-      this.speechBubble.destroy();
-      this.speechBubble = undefined;
-    }
-    if (this.speechBubbleText) {
-      this.speechBubbleText.destroy();
-      this.speechBubbleText = undefined;
-    }
-
     this.cat.job = {
       x: this.food.sprite.x,
       type: NPCJobType.RUN,
@@ -269,25 +150,6 @@ export class BaseScene extends Scene {
 
   update() {
     this.cat?.update();
-    if (this.cat && this.speechBubble && this.speechBubbleText) {
-      const catX = this.cat.sprite.x;
-      const catY = this.cat.sprite.y;
-
-      const bubbleWidth = 150;
-      const bubbleHeight = 35;
-
-      const bubbleX = catX - bubbleWidth / 2 + 50;
-      const bubbleY = catY - this.cat.sprite.height - bubbleHeight + 25;
-
-      this.speechBubble.setPosition(bubbleX, bubbleY);
-
-      const b = this.speechBubbleText.getBounds();
-
-      this.speechBubbleText.setPosition(
-        bubbleX + bubbleWidth / 2 - b.width / 2,
-        bubbleY + bubbleHeight / 2 - b.height / 2
-      );
-    }
   }
 
   private onFoodEat() {
@@ -297,15 +159,6 @@ export class BaseScene extends Scene {
       this.food = null;
       this.sound.play("purr", { volume: 0.5 });
       GameEvents.CAT_EATEN.push();
-      if (this.cat) {
-        this.createSpeechBubble(
-          this.cat.sprite.x,
-          this.cat.sprite.y - 50,
-          150,
-          35,
-          afterFeedMessag
-        );
-      }
     });
   }
 }
---
+++ components/shared/SpeechBubble.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { getRandomItemFromArray } from "@/constants/utils";
+import { greetingText } from "@/constants/utils";
+import { aftrFeedText } from "@/constants/utils";
+import { useCat } from "@/context/CatContext";
+const greetingMessage = getRandomItemFromArray(greetingText);
+const afterFeedMessage = getRandomItemFromArray(aftrFeedText);
+export const SpeechBubble = () => {
+    const { cat } = useCat();
+    const [displayText, setDisplayText] = useState("");
+    const [typingIndex, setTypingIndex] = useState(0);
+    const [isVisible, setIsVisible] = useState(true);
+    const isCatFedded = cat?.status.EAT === 4
+    const currentMessageToDisplay = isCatFedded ? afterFeedMessage : greetingMessage
+
+    useEffect(() => {
+        setIsVisible(false);
+        const timeout = setTimeout(() => {
+            setIsVisible(true);
+            setDisplayText("");
+            setTypingIndex(0);
+        }, 500);
+
+        return () => clearTimeout(timeout);
+    }, [isCatFedded]);
+
+    useEffect(() => {
+        if (isVisible && typingIndex < currentMessageToDisplay!.length) {
+            const timeout = setTimeout(() => {
+                setDisplayText((prev) => prev + currentMessageToDisplay![typingIndex]);
+                setTypingIndex((prev) => prev + 1);
+            }, 50);
+            return () => clearTimeout(timeout);
+        }
+    }, [typingIndex, isVisible, currentMessageToDisplay]);
+
+    return (
+        <div className="absolute h-screen w-screen">
+            {isVisible && (
+                <div
+                    className={`absolute top-[38%] left-1/2 transform translate-x-1 text-center text-sm text-black bg-white ${isCatFedded ? "p-px" : "p-3"
+                        } lg:w-60 w-40 shadow-[0_-4px_#fff,0_-8px_#000,4px_0_#fff,4px_-4px_#000,8px_0_#000,0_4px_#fff,0_8px_#000,-4px_0_#fff,-4px_4px_#000,-8px_0_#000,-4px_-4px_#000,4px_4px_#000]`}
+                >
+                    <p className="font-quanternary lg:text-p5 text-p7 font-semibold">
+                        {displayText}
+                    </p>
+                    <div className="absolute bottom-[-6px] left-[32px] lg:h-1 lg:w-1 h-1.5 w-1.5 bg-white shadow-custom"></div>
+                </div>
+            )}
+        </div>
+    )
+}
