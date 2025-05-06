f2c1890253b9a9cc6415c9e788be31002d600086
Žygimantas Bagdzevičius <zbagdzevicius@gmail.com>
2025-05-06 09:51:09 +0300

    camp update

--- models/para.ts
+++ models/para.ts
@@ -2,5 +2,6 @@ import Para, { Environment } from "@getpara/react-sdk";
 
 export const para = new Para(
   Environment.BETA,
-  "c8d0894b02c6c07632baa47da13edc37"
+  "c8d0894b02c6c07632baa47da13edc37",
+  { externalWalletConnectionOnly: true }
 );
