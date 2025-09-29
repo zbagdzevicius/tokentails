0bf0d8be38acf6abc2103880e0f383cd253435c9
Žygimantas Bagdzevičius <zbagdzevicius@gmail.com>
2025-09-29 18:20:48 +0300

    reduce gas fees

--- evm/TokenTailsCat.sol
+++ evm/TokenTailsCat.sol
@@ -2,35 +2,41 @@
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts@4.9.3/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.9.3/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
 import "@openzeppelin/contracts@4.9.3/utils/Strings.sol";
 
-struct PlayData {
-    uint32 playCount;
-    uint32 lastPlayDate;
-}
-
-/// @notice Custom errors (cheaper than revert strings)
 error NotOwner();
 error AlreadyIncremented();
 error NonexistentToken();
+error NotMinter();
 
-contract TokenTailsCat is ERC721, AccessControl {
-    string private _baseTokenURI;
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    mapping(uint256 => PlayData) public playData;
+contract TokenTailsCat is ERC721, Ownable {
+    mapping(address => bool) public minter;
+    event MinterAdded(address indexed account);
+    event MinterRemoved(address indexed account);
+    event Played(address indexed player, uint256 indexed tokenId, uint32 day);
+
+    mapping(uint256 => uint64) public playData;
     string private constant BASE = "https://api.tokentails.com/cat/nft/";
 
     constructor() ERC721("Token Tails Cat", "TTCAT") {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(MINTER_ROLE, _msgSender());
+        minter[msg.sender] = true;
     }
 
-    function mintUniqueTokenTo(
-        address to,
-        uint256 tokenId
-    ) external onlyRole(MINTER_ROLE) {
+    function mintUniqueTokenTo(address to, uint256 tokenId) external {
+        if (!minter[msg.sender]) revert NotMinter();
         _mint(to, tokenId);
+    }
+
+    function addMinter(address account) external onlyOwner {
+        minter[account] = true;
+        emit MinterAdded(account);
+    }
+
+    function removeMinter(address account) external onlyOwner {
+        if (!minter[account]) revert NotMinter();
+        delete minter[account];
+        emit MinterRemoved(account);
     }
 
     function tokenURI(
@@ -40,29 +46,32 @@ contract TokenTailsCat is ERC721, AccessControl {
         return string(abi.encodePacked(BASE, Strings.toString(tokenId)));
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view override(ERC721, AccessControl) returns (bool) {
-        return super.supportsInterface(interfaceId);
-    }
-
     function incrementPlayCount(uint256 tokenId) external {
         if (ownerOf(tokenId) != msg.sender) revert NotOwner();
 
-        uint32 today = uint32(block.timestamp / 86400); // Calculate current day as an integer
-        if (playData[tokenId].lastPlayDate == today)
-            revert AlreadyIncremented();
+        uint32 today = uint32(block.timestamp / 86400);
+        uint64 packed = playData[tokenId];
 
+        uint32 last = uint32(packed >> 32);
+        if (last == today) revert AlreadyIncremented();
+
+        uint32 count = uint32(packed);
         unchecked {
-            playData[tokenId].playCount += 1;
+            count += 1;
         }
-        playData[tokenId].lastPlayDate = today;
+
+        playData[tokenId] = (uint64(today) << 32) | uint64(count);
     }
 
     function getPlayData(
         uint256 tokenId
     ) external view returns (uint32 playCount, uint32 lastPlayDate) {
-        PlayData memory d = playData[tokenId];
-        return (d.playCount, d.lastPlayDate);
+        uint64 p = playData[tokenId];
+        return (uint32(p), uint32(p >> 32));
+    }
+
+    function checkIn(uint256 tokenId) external {
+        uint32 today = uint32(block.timestamp / 86400);
+        emit Played(msg.sender, tokenId, today);
     }
 }
