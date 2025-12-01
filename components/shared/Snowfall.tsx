47e0b22cb67c07bafe222da264fa04bf60d90bb2
Žygimantas Bagdzevičius <zbagdzevicius@gmail.com>
2025-12-01 22:45:20 +0200

    revert hearts

--- components/shared/Snowfall.tsx
+++ components/shared/Snowfall.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Snowfall: React.FC<{ isSmall?: boolean }> = ({ isSmall = false }) => {
   return (
-    <div className="absolute snowfall inset-0 w-screen h-screen overflow-hidden z-0 pointer-events-none">
+    <div className="absolute inset-0 w-screen h-screen overflow-hidden z-0 pointer-events-none">
       {Array.from({ length: 50 }).map((_, i) => (
-        <div className={`snowflake ${isSmall ? "small" : ""}`} key={i} />
+        <div className={`chamomile-flower ${isSmall ? "small" : ""}`} key={i} />
       ))}
     </div>
   );
